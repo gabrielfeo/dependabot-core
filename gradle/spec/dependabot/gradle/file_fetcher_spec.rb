7b7132ac385477c32af672f619ca6e4bff4556f9
Gabriel Feo <gabriel@gabrielfeo.com>
2022-04-21 09:48:14 +0100

    fixup! Add failing test on file_fetcher

--- gradle/spec/dependabot/gradle/file_fetcher_spec.rb
+++ gradle/spec/dependabot/gradle/file_fetcher_spec.rb
@@ -80,18 +80,26 @@ RSpec.describe Dependabot::Gradle::FileFetcher do
       end
     end
 
-    context "with included builds" do
+    context "with included build" do
       before do
-        # TODO
-        stub_content_request("?ref=sha", "contents_java_with_settings.json")
-        stub_content_request("settings.gradle?ref=sha", "contents_java_simple_settings.json")
+        stub_content_request("?ref=sha", "contents_java_composite_with_settings.json")
+        stub_content_request("settings.gradle?ref=sha", "contents_java_composite_settings.json")
+        stub_content_request("build.gradle?ref=sha", "contents_java_basic_buildfile.json")
         stub_content_request("app/build.gradle?ref=sha", "contents_java_basic_buildfile.json")
+        stub_content_request("included/settings.gradle?ref=sha", "contents_java_simple_settings.json")
+        stub_content_request("included/build.gradle?ref=sha", "contents_java_basic_buildfile.json")
+        stub_content_request("included/app/build.gradle?ref=sha", "contents_java_basic_buildfile.json")
       end
 
-      it "fetches main buildfile and subproject buildfile of included build" do
+      it "fetches all buildfiles of main and included builds" do
         expect(file_fetcher_instance.files.count).to eq(5)
         expect(file_fetcher_instance.files.map(&:name)).
-          to match_array(%w(build.gradle settings.gradle included/build.gradle included/settings.gradle included/subproj/build.gradle))
+          to match_array(%w(
+            build.gradle settings.gradle
+            app/build.gradle
+            included/build.gradle included/settings.gradle
+            included/app/build.gradle
+          ))
       end
     end
 
